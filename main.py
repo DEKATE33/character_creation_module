d1af4be2c1935f2397990b1fb56f4f1746c99a6e
Артем Дириженко <dekate33@outlook.com>
2023-08-14 13:30:41 +0300

    Add module start_game_banner.py and docstring in all files

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+"""Эта игра."""
+
 from random import randint
+
+from graphic_arts.start_game_banner import run_screensaver
 
 
 # Функция атаки.
 def attack(char_name: str, char_class: str) -> str:
+    """Функция атаки."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный'
                 f' {5 + randint(3, 5)}')
@@ -17,6 +22,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 # Функция защиты.
 def defence(char_name: str, char_class: str) -> str:
+    """Функция защиты."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -28,6 +34,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 # Функция бафа.
 def special(char_name: str, char_class: str) -> str:
+    """Функция бафа."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение «Выносливость'
                 f' {80 + 25}»')
@@ -41,6 +48,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Функция тренировки команд для игры."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -65,6 +73,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Функция выбора персонажа."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -86,7 +95,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
